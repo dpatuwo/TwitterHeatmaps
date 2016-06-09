d4f3ea35b72ffcf6a61108e86a4e11e127d332d9
David Patuwo <dpatuwo@kent.edu>
2016-06-09 11:14:51 -0400

    Twitter now responding, mustache not working

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var Twitter = require('Twitter')
+// var Twitter = require('Twitter')
+var Twit = require('twit')
 var GoogleMapsAPI = require('googlemaps')
 var cn = require('chuck-norris-api')
 var cowsay = require('cowsay')
 var pry = require('pryjs')
 // var bootstrap = require('bootstrap')
-// var jquery = require('jquery')
+var jQuery = require('jquery')
 var express = require('express')
 var app = express()
 var mustacheExpress = require('mustache-express')
@@ -14,12 +15,19 @@ app.set('view engine','html')
 app.set('views',__dirname+'/html')
 app.use(express.static(__dirname+'public'))
 
-var client = new Twitter({
+// var client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+// });
+
+var T = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
 
 var publicConfig = {
   key: process.env.GOOGLEMAPS_API_KEY,
@@ -40,7 +48,22 @@ function talkCow(text){
 app.listen(3000, function(){
   console.log('App is listening on port 3000!')
 })
+var tweet = ''
+T.get('search/tweets', { q: '#shakeshack since:2011-07-11', count: 10 }, function(err, data, response) {
+  // console.log(data)
+  var tweets = []
+  for(var i = 0; i < data.statuses.length; ++i){
+    if(data.statuses[i].geo != null){
+      tweets.push(data.statuses[i])
+    }
+  }
+  //console.log(tweets)
+  eval(pry.it)
+})
+
 
 app.get('/', function(req, res){
-  res.send("Hello World!")
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
+
+
